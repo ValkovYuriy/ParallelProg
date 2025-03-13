ea597e2fa6fd049be7fcfe8ff94f70db69ee37df
Kirill <kirill5151122@gmail.com>
2025-03-13 15:20:41 +0400

    Fix: Думайте сами, решайте сами иметь или не иметь...

--- TaskBag.cpp
+++ TaskBag.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <queue>
 #include <tchar.h>
 #include "tbag.h"
@@ -7,10 +8,10 @@
 
 #define COUNT 1000
 
-const int N = 10000;
-const int P = 6;
+int N = 10000;
+int P = 2;
 const int T = 10;
-int arr[N];
+
 
 struct task {
     int *a;
@@ -92,13 +93,13 @@ public:
     bag b{};
     double real_time_milliseconds = 0.0;
 
-    explicit TaskBag(int num_prc) : TBag(num_prc) {
+    explicit TaskBag(int num_prc, int *arr) : TBag(num_prc) {
         struct timespec start{}, end{};
         unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
         std::mt19937 generator(seed);
         std::uniform_int_distribution<int> distribution(1, N);
-        for (int &i: arr) {
-            i = distribution(generator);
+        for (int i=0; i< N; i++) {
+            arr[i] = distribution(generator);
         }
         clock_gettime(CLOCK_REALTIME, &start);
         qSort0(&b, arr, N);
@@ -130,19 +131,52 @@ public:
 };
 
 int _tmain() {
-/*    TaskBag bag(P);
-    bag.run();
-    //std::cout<<"\nspeedup = "<<bag.speedup();//1-при логической отладке,
-
-    for (int i = 0; i < N; i++) std::cout << i << ") " << arr[i] << '\n';
-    std::cout << "\nduration = " << bag.duration() << " sec\n";*/
-    double real_time_milliseconds = 0.0;
-    for (int count = 0; count < COUNT; count++) {
-        TaskBag bag(P);
-        bag.run();
-        real_time_milliseconds += bag.duration() * 1000 + (double) bag.real_time_milliseconds;
+    std::ofstream outputFile("results.csv");
+    if (!outputFile.is_open()) {
+        std::cerr << "File error" << std::endl;
+        return 1;
     }
-    printf("AVG exec time: %f ms\n", real_time_milliseconds / COUNT);
+    outputFile << "N,P,AVG_SERIAL_TIME,AVG_PARALLEL_TIME\n";
+    for (; N <= 10000000; N *= 10) {
+        printf("N: %d\n", N);
+        int arr[N];
+        double real_time_milliseconds = 0.0;
+        for (int count = 0; count < COUNT; count++) {
+            struct timespec start{}, end{};
+            unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+            std::mt19937 generator(seed);
+            std::uniform_int_distribution<int> distribution(1, N);
+            for (int &i: arr) {
+                i = distribution(generator);
+            }
+            clock_gettime(CLOCK_REALTIME, &start);
+            qSort(arr, N);
+            clock_gettime(CLOCK_REALTIME, &end);
+            double real_time = (double) (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
+            real_time_milliseconds += real_time * 1000;
+        }
+        double serial_time = real_time_milliseconds / COUNT;
+        printf("AVG serial time: %f ms\n", serial_time);
+        for (P = 2; P <= 8; P *= 2) {
+            real_time_milliseconds = 0.0;
+            printf("P: %d\n", P);
+            for (int count = 0; count < COUNT; count++) {
+                unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+                std::mt19937 generator(seed);
+                std::uniform_int_distribution<int> distribution(1, N);
+                for (int &i: arr) {
+                    i = distribution(generator);
+                }
+                int *ptr = arr;
+                TaskBag bag(P, ptr);
+                bag.run();
+                real_time_milliseconds += bag.duration() * 1000 + (double) bag.real_time_milliseconds;
+            }
+            double parallel_time = real_time_milliseconds / COUNT;
+            printf("AVG parallel time: %f ms\n", parallel_time);
+            outputFile << N << "," << P  << "," << serial_time << ","  << parallel_time << "\n";
+        }
+    }
+    outputFile.close();
     return 0;
 }
-
